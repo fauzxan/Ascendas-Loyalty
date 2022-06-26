0369205d1e9dea883fe64175fc526602bff883a5
anthonylim0 <anthony.rclim@gmail.com>
2022-06-26 21:35:50 +0800

    Popup

--- client/src/components/PartnerCardSingular.js
+++ client/src/components/PartnerCardSingular.js
@@ -1,20 +1,33 @@
 import React from "react";
 import "./styles/partnerCard.scss";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import Modal from "./Modal";
+import { useState } from "react";
 
 function PartnerCard(props) {
-	// all the cards below are sample cards
+  // all the cards below are sample cards
 
-	return (
-		<div className="card">
-			<div className="card__body">
-				<img src={props.img} className="card__image" alt="company logo" />
-				<h2 className="card__title">{props.title}</h2>
-				<p className="card__description">{props.desc} </p>
-			</div>
-			<Button variant="contained" className="card__button">Get Rewards</Button>
-		</div>
-	);
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <div className="card">
+      <div className="card__body">
+        <img src={props.img} className="card__image" alt="company logo" />
+        <h2 className="card__title">{props.title}</h2>
+        <p className="card__description">{props.desc} </p>
+      </div>
+      <Button
+        variant="contained"
+        className="card__button"
+        onClick={() => {
+          setOpenModal(true);
+        }}
+      >
+        Get Rewards
+      </Button>
+      {openModal && <Modal closeModal={setOpenModal} />}
+    </div>
+  );
 }
 
 export default PartnerCard;
