9d1674d2cee6289881bfcac7e97bf6b694ddd9ab
anthonylim0 <anthony.rclim@gmail.com>
2022-06-26 23:44:44 +0800

    Popup modal

--- client/src/components/PartnerCardSingular.js
+++ client/src/components/PartnerCardSingular.js
@@ -1,33 +1,48 @@
 import React from "react";
 import "./styles/partnerCard.scss";
-import Button from "@mui/material/Button";
-import Modal from "./Modal";
 import { useState } from "react";
+import { Button, Modal } from "antd";
+import "antd/dist/antd.css";
 
-function PartnerCard(props) {
+const PartnerCardSingular = (props) => {
+  console.log(props);
   // all the cards below are sample cards
 
-  const [openModal, setOpenModal] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
   return (
     <div className="card">
       <div className="card__body">
-        <img src={props.img} className="card__image" alt="company logo" />
-        <h2 className="card__title">{props.title}</h2>
-        <p className="card__description">{props.desc} </p>
+        <img src={props.card.img} className="card__image" alt="company logo" />
+        <h2 className="card__title">{props.card.title}</h2>
+        <p className="card__description">{props.card.desc} </p>
       </div>
-      <Button
-        variant="contained"
-        className="card__button"
-        onClick={() => {
-          setOpenModal(true);
-        }}
-      >
-        Get Rewards
+      <Button type="primary" onClick={showModal}>
+        Claim rewards
       </Button>
-      {openModal && <Modal closeModal={setOpenModal} />}
+      <Modal
+        title="Basic Modal"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </div>
   );
-}
+};
 
-export default PartnerCard;
+export default PartnerCardSingular;
