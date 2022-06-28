bd6d613fde5efe52253f1a29398248262afa078a
cyx <ggalrz@gmail.com>
2022-06-28 20:54:01 +0800

    Database configured

--- server/db/config.js
+++ server/db/config.js
@@ -1,2 +1,6 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/loyalty")
+mongoose.connect('mongodb+srv://cyx:loyalty@ascenda-loyalty.pehi0t8.mongodb.net/loyalty?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+    console.log('Connected to database');
+}).catch((err)=>{
+    console.error(`Error connecting to the database. n${err}`);
+});
