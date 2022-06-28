015ce06d60e793445bddc883bc483532d1913342
cyx <ggalrz@gmail.com>
2022-06-28 14:12:38 +0800

    signupForm --> "Enabled registration with mongo"

--- client/src/components/loginPage/signupForm.jsx
+++ client/src/components/loginPage/signupForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Marginer } from "./marginer";
 import {
   BoldLink,
@@ -8,21 +8,45 @@ import {
   MutedLink,
   SubmitButton,
 } from "./common";
-import { AccountContext } from "./context";
+import { AccountContext, loginContext } from "./context";
+import { useNavigate } from 'react-router-dom';
 
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const setLoggedIn = useContext(loginContext);
+
+  const signup = async()=>{
+    console.warn(name, email, password);
+    let result = await fetch("http://localhost:5000/register", {
+      method:'post',
+      body:JSON.stringify({name, email, password}),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    result = await result.json();
+    console.warn(result);
+    setLoggedIn(true);
+    navigate("/Home")
+  }
 
   return (
     <BoxContainer>
       <FormContainer>
-        <Input type="text" placeholder="Full Name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+        <Input type="text" placeholder="Full Name" 
+        value={name} onChange={(e)=>setName(e.target.value)}/>
+        <Input type="email" placeholder="Email" 
+        value={email} onChange={(e)=>setEmail(e.target.value)}/>
+        <Input type="password" placeholder="Password" 
+        value={password} onChange={(e)=>setPassword(e.target.value)}/>
         <Input type="password" placeholder="Confirm Password" />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
-      <SubmitButton type="submit">Signup</SubmitButton>
+      <SubmitButton type="submit" onClick={signup}>Signup</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Already have an account?
