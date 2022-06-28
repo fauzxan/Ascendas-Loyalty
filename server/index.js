d28fb4d2335bf35ecca951233de50ae8cc15b214
cyx <ggalrz@gmail.com>
2022-06-28 16:25:20 +0800

    index.js --> "Added login API"

--- server/index.js
+++ server/index.js
@@ -7,10 +7,26 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-app.post("/register", async (req, res)=>{
+app.post("/register", async (req, res) => {
   let user = new User(req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   res.send(result);
+})
+
+app.post("/login", async (req, res) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select('-password');
+    if (user) {
+      res.send(user)
+    } else {
+      res.send({ result: "Not found" });
+    }
+  } else {
+    res.send({ result: "Not found" });
+  }
+
 })
 
 app.listen(5000);
