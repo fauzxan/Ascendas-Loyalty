2c39c1abc8a4945ba9451ea244b644cbf4c942a3
Fauzaan <fauzaan.mm@gmail.com>
2022-07-13 00:13:31 +0800

    contentTransfer.js->"attempt 1"

--- server/handback/contentTransfer.js
+++ server/handback/contentTransfer.js
@@ -4,13 +4,14 @@
 // 4. import the randomized outcome code into the handback document inside mongodb, such that it overwrites the existing content with fresh content
 
 //1.
+/*
 const mongodb = require("mongodb").MongoClient;
 const fastcsv = require("fast-csv");
 const fs = require("fs");
 
 let uniqueTime = new Date().toLocaleTimeString();
 let uniqueDate = new Date().toLocaleDateString();
-let fileName = `./server/rwStream/accrual.csv`;
+let fileName = `ftp://sutd_2022_c4g7:rxh3qpj7man0qwz_CNZ@ftp.drivehq.com/accrual.csv`;
 const writeStream = fs.createWriteStream(fileName);
 let url =
 	"mongodb+srv://c4g7:loyalty@ascenda-loyalty.684oztw.mongodb.net/loyalty?retryWrites=true&w=majority";
@@ -38,3 +39,51 @@ mongodb.connect(
 			});
 	}
 );
+*/
+var fs = require("fs"),
+	client = require("ssh2").Client; // creating the ssh2 connection variable 
+
+upoladFile({
+	localFolder:
+		"C:\\Users\\fauza\\Desktop\\School stuff\\Elements of Software Construction\\Ascendas-Loyalty\\server\\rwStream\\",
+	localFile: "accrual.csv",
+	remoteFolder: "\\DriveHQShare\\melvrickgoh\\sutd_2022_c4g7\\",
+	remoteFile: "accrual.csv",
+	host: "proFTP.drivehq.com",
+	port: 22,
+	username: "sutd_2022_c4g7",
+	password: "rxh3qpj7man0qwz_CNZ",
+});
+
+function upoladFile(p) {
+	var conn = new client();
+	conn
+		.on("ready", function () {
+			conn.sftp(function (err, sftp) {
+				if (err) throw err;
+				sftp.readdir(p.remoteFolder, function (err, list) {
+					if (err) throw err;
+					console.dir(list);
+					var readStream = fs.createReadStream(p.localFolder + p.localFile);
+					var writeStream = sftp.createWriteStream(
+						p.remoteFolder + p.remoteFile,
+						{ mode: 0100664 }
+					);
+					writeStream.on("close", function () {
+						sftp.readdir(p.remoteFolder, function (err, list) {
+							if (err) throw err;
+							console.dir(list);
+							conn.end();
+						});
+					});
+					readStream.pipe(writeStream);
+				});
+			});
+		})
+		.connect({
+			host: p.host,
+			port: p.port,
+			username: p.username,
+			password: p.password,
+		});
+}
