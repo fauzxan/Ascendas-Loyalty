690cad8eec131551dd15bf1391b3af2cbf661689
cyx <ggalrz@gmail.com>
2022-07-16 18:55:00 +0800

    creditReq --> "refcode now in format yyyymmdd(i)"
    i is the total number of requests in the considered timeframe, including the current one

--- server/index.js
+++ server/index.js
@@ -3,16 +3,17 @@ require("./db/config");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+global.curr = 0;
 
 const TcModel = require("./models/Ascendas_transfer_connect");
 
-const cors = require("cors");// allows us to connect to the react frontend
+const cors = require("cors"); // allows us to connect to the react frontend
 const login = require("./routes/login");
 const register = require("./routes/register");
 const creditreq = require("./routes/creditReq");
 const handback = require("./routes/hanbackRoute");
 
-const creditreqModel = require('./db/creditReq');
+const creditreqModel = require("./db/creditReq");
 
 //external modules
 app.use(express.json());
@@ -25,16 +26,16 @@ app.use("/submitcreditreq", creditreq);
 app.use("/createhandback", handback);
 
 app.listen(5000, () => {
-	console.log("Server is listening");
+  console.log("Server is listening");
 });
 
 // get method to retrieve data from creditreq:
 app.get("/getcreditreq", (req, res) => {
-	creditreqModel.find({}, (err, data) => {
-		if (err) {
-			res.json(err);
-		} else {
-			res.json(data);
-		}
-	});
+  creditreqModel.find({}, (err, data) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(data);
+    }
+  });
 });
--- server/routes/creditReq.js
+++ server/routes/creditReq.js
@@ -3,11 +3,17 @@ const express = require("express");
 const creditReq = require("../db/creditReq");
 const router = express.Router();
 
-
 router.post("/", async (req, res) => {
-	let request = new creditReq(req.body);
-	let result = await request.save();
-	res.send(result);
+  const today = new Date();
+  console.log(today.getMonth());
+  req.body["refcode"] = parseInt(
+    `${today.getFullYear()}${today.getMonth() + 1}${today.getDate()}${curr}`
+  );
+  console.log(today.getFullYear() + today.getMonth() + today.getDate() + curr);
+  let request = new creditReq(req.body);
+  let result = await request.save();
+  curr += 1;
+  res.send(result);
 });
 
 module.exports = router;
