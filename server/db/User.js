c835fbe462255bbc4b48ec9c6385a09c1e61b6bf
cyx <ggalrz@gmail.com>
2022-07-17 14:24:17 +0800

    User --> "Fixed transactions"

--- server/db/User.js
+++ server/db/User.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    transactions: Map
-})
+  name: String,
+  email: String,
+  password: String,
+  transactions: {
+    type: Map,
+    default: {},
+  },
+});
 
-module.exports = mongoose.model("users", userSchema)
+module.exports = mongoose.model("users", userSchema);
