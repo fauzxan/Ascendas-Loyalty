0b9b36910adf60318ac94be7e509a2745e07cc6e
cyx <ggalrz@gmail.com>
2022-07-17 12:37:38 +0800

    User --> "Added transactions"

--- server/routes/creditReq.js
+++ server/routes/creditReq.js
@@ -1,15 +1,20 @@
 require("../db/config");
 const express = require("express");
 const creditReq = require("../db/creditReq");
+const User = require("../db/User");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   const today = new Date();
-  console.log(today.getMonth());
+  console.log(req.body.email)
+  await User.findOneAndUpdate({email: req.body.email}, {
+    $set: {
+      [`transactions.${req.body.refcode}`]: "69",
+    }
+  });
   req.body["refcode"] = parseInt(
     `${today.getFullYear()}${today.getMonth() + 1}${today.getDate()}${curr}`
   );
-  console.log(today.getFullYear() + today.getMonth() + today.getDate() + curr);
   let request = new creditReq(req.body);
   let result = await request.save();
   curr += 1;
