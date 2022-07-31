2a21487a30d880dce1874fd2572471c9aa7bfd16
anthonylim0 <anthony.rclim@gmail.com>
2022-07-31 20:16:13 +0800

    Bank -> "Button align"

--- client/src/components/bankLanding/HomeList.js
+++ client/src/components/bankLanding/HomeList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "antd";
 import { Carousel } from "antd";
+import { Col, Row } from "antd";
 import { useNavigate } from "react-router-dom";
 import "./bank.css";
 
@@ -19,50 +20,56 @@ const HomeList = (props) => {
   var button_id = "bank-" + props.id;
   return (
     <div className="backgnd">
-      <div className="carousel">
-        <h2 className="user">Welcome Back,</h2>
-        <h1>{user}</h1>
-        <div style={{}}>
-          <Carousel autoplay speed={3500}>
-            <div className="wrap">
-              <h3 style={contentStyle}>
-                Emails and SMSes from DBS will not include clickable links.
-                Always go directly to our website to verify the authenticity of
-                any banking-related requests or offers.
-              </h3>
+      <div>
+        <h2 className="greeting">Welcome Back,</h2>
+        <Row>
+          <Col span={12}>
+            <h1 className="user">{user}</h1>
+            <div className="carousel">
+              <Carousel autoplay speed={3500}>
+                <div>
+                  <h3 style={contentStyle}>
+                    Emails and SMSes from DBS will not include clickable links.
+                    Always go directly to our website to verify the authenticity
+                    of any banking-related requests or offers.
+                  </h3>
+                </div>
+                <div>
+                  <h3 style={contentStyle}>
+                    DBS will never ask you for your credit or debit card
+                    details, CVV, SMS or email OTPs, or Digital Token approvals
+                    to verify or unlock your account.
+                  </h3>
+                </div>
+                <div>
+                  <h3 style={contentStyle}>
+                    Do not call phone numbers, click on URL links, or scan QR
+                    codes in unsolicited emails, SMS, or other Messaging
+                    Application messages.
+                  </h3>
+                </div>
+                <div>
+                  <h3 style={contentStyle}>
+                    Never disclose your card numbers or OTPs to unverified
+                    sources. Bank staff and government officials will never
+                    request your card details, OTPs, or Digital Token Approvals
+                    through SMS, voice calls, or unofficial websites.
+                  </h3>
+                </div>
+              </Carousel>
             </div>
-            <div>
-              <h3 style={contentStyle}>
-                DBS will never ask you for your credit or debit card details,
-                CVV, SMS or email OTPs, or Digital Token approvals to verify or
-                unlock your account.
-              </h3>
-            </div>
-            <div>
-              <h3 style={contentStyle}>
-                Do not call phone numbers, click on URL links, or scan QR codes
-                in unsolicited emails, SMS, or other Messaging Application
-                messages.
-              </h3>
-            </div>
-            <div>
-              <h3 style={contentStyle}>
-                Never disclose your card numbers or OTPs to unverified sources.
-                Bank staff and government officials will never request your card
-                details, OTPs, or Digital Token Approvals through SMS, voice
-                calls, or unofficial websites.
-              </h3>
-            </div>
-          </Carousel>
-          <Button
-            id={button_id}
-            onClick={() => navigate("/bank-" + props.id)}
-            type="danger"
-            className="card_rewards__btn hover"
-          >
-            View Available Rewards
-          </Button>
-        </div>
+          </Col>
+          <Col span={12}>
+            <Button
+              id={button_id}
+              onClick={() => navigate("/bank-" + props.id)}
+              type="danger"
+              className="card_rewards__btn hover"
+            >
+              View Available Rewards
+            </Button>
+          </Col>
+        </Row>
       </div>
     </div>
   );
