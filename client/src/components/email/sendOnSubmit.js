5e5d22357ab5df9feacccc19da276c246d1c7c08
Fauzaan <fauzaan.mm@gmail.com>
2022-08-02 13:42:13 +0800

    sendOnSubmit.js->"toString()"

--- client/src/components/email/sendOnSubmit.js
+++ client/src/components/email/sendOnSubmit.js
@@ -7,7 +7,7 @@ async function sendOnSubmit (values){
         to_name: localStorage.getItem("user"),
         points: values.amount,
         membership_number: values.membership_number,
-        user_email: localStorage.getItem("email")
+        user_email: localStorage.getItem("email").toString() 
     }
     await emailjs.send('contact_service', 'contact_form', obj, 'dBjhmcgLenXD47XDp').then(result => {
         console.log("email has been sent")
