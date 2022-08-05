2d2d74d4c49b1e83240c72eb6128893915ba83cb
cyx <ggalrz@gmail.com>
2022-08-06 01:11:37 +0800

    TransferConnect --> "Added membership validation API"

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const handback = require("./routes/hanbackRoute");
 const LoyaltyProgram = require("./routes/loyaltyprogram");
 const LoyaltyUpload = require("./routes/loyaltyUpload");
 const intermdiaryFind = require("./routes/getIntermediate");
+const mv = require("./routes/mv");
 
 const creditreqModel = require("./db/creditReq");
 const { makeHandback } = require("./Dailies/handback");
@@ -23,29 +24,30 @@ app.use("/createhandback", handback);
 app.use("/getloyaltyprogram", LoyaltyProgram);
 app.use("/loyaltyupload", LoyaltyUpload);
 app.use("/getintermediate", intermdiaryFind);
+app.use("/validate", mv);
 
 app.get("/makeacc", (req, res) => {
-	makeAccural();
-	res.status(200).send("ok");
+  makeAccural();
+  res.status(200).send("ok");
 });
 
 app.get("/makehb", (req, res) => {
-	// code to make handback file
-	makeHandback();
-	res.status(200).send("ok");
+  // code to make handback file
+  makeHandback();
+  res.status(200).send("ok");
 });
 
 app.listen(5000, () => {
-	console.log("Server is listening");
+  console.log("Server is listening");
 });
 
 // get method to retrieve data from creditreq:
 app.get("/getcreditreq", (req, res) => {
-	creditreqModel.find({}, (err, data) => {
-		if (err) {
-			res.json(err);
-		} else {
-			res.json(data);
-		}
-	});
+  creditreqModel.find({}, (err, data) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(data);
+    }
+  });
 });
